9a28688d84c35a42956131ea3e6249d9f933daa9
Robert Smith <quad@symbo1ics.com>
2012-05-26 23:39:15 -0500

    Change argument order of ATAN-R to match CL:ATAN's order. Also some minor re-indentations.
    Thanks to Paul Khuong for the suggestion.

--- constants.lisp
+++ constants.lisp
@@ -3,7 +3,7 @@
 (defvar +LOG2-R+ (+r (ash-r (log-r2 1/7) 1) (log-r2 1/17))
   "log(2) as CREAL")
 
-(get-approx +log2-r+ 200)  ; precompute to ca. 60 decimal digits
+(get-approx +log2-r+ 200)           ; compute to ca. 60 decimal digits
 
 (defvar +PI-R+ (-r (ash-r (atan-r1 1/10) 5)
                    (ash-r (atan-r1 1/515) 4)
@@ -14,7 +14,7 @@
 (defvar +PI/2-R+ (ash-r +pi-r+ -1) "pi/2 as CREAL")
 (defvar +PI/4-R+ (ash-r +pi-r+ -2) "pi/4 as CREAL")
 
-(get-approx +2pi-r+ 200) ; compute to ca. 60 decimal digits
+(get-approx +2pi-r+ 200)            ; compute to ca. 60 decimal digits
 (get-approx +pi-r+ 200)
 (get-approx +pi/2-r+ 200)
 (get-approx +pi/4-r+ 200)
